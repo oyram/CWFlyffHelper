c62e45a0158a84594a5b2becf2493f074d42bc3f
oyram <5166476+oyram@users.noreply.github.com>
2022-03-19 08:46:10 +0100

    .ui removed, presets added, edit row button rem.

--- source/cwflyffhelper.py
+++ source/cwflyffhelper.py
@@ -27,14 +27,14 @@ import os.path
 # launch game with presets
 # launch patcher (to update the game)
 
-import pathlib
+# import pathlib
 import pygubu
 import tkinter as tk
 import tkinter.ttk as ttk
 from pygubu.widgets.pathchooserinput import PathChooserInput
 
-PROJECT_PATH = pathlib.Path(__file__).parent
-PROJECT_UI = PROJECT_PATH / "CWFlyffHelper.ui"
+# PROJECT_PATH = pathlib.Path(__file__).parent
+# PROJECT_UI = PROJECT_PATH / "CWFlyffHelper.ui"
 
 #incomplete. 3rd column in treeview with suggestions / min and max values; i.e. tooltips
 # iniValues = ["1920 1080", "0=no,1=yes", "2=low, 1=mid, 0=high", "2=low, 1=mid, 0=high",
@@ -88,17 +88,18 @@ class CwflyffhelperApp:
         self.treeviewIniSettings.bind('<<TreeviewSelect>>', self.event_treeviewIniSettings)
         self.entryIniSetting = ttk.Entry(self.frameIniSettings)
         self.entryIniSetting.grid(column='2', padx='5', pady='5', row='1')
-        self.buttonEditIniSetting = ttk.Button(self.frameIniSettings)
-        self.buttonEditIniSetting.configure(text='Edit Row', command=self.edittreeviewIniSettings)
-        self.buttonEditIniSetting.grid(column='3', padx='5', pady='5', row='4')
+        self.entryIniSetting.bind('<Return>', self.edittreeviewIniSettings)
+        # self.buttonEditIniSetting = ttk.Button(self.frameIniSettings)
+        # self.buttonEditIniSetting.configure(text='Edit Row', command=self.edittreeviewIniSettings)
+        # self.buttonEditIniSetting.grid(column='3', padx='5', pady='5', row='4')
         self.buttonSaveIniPreset = ttk.Button(self.frameIniSettings)
-        self.buttonSaveIniPreset.configure(text='Save Preset')
+        self.buttonSaveIniPreset.configure(text='Save Preset', command=self.savePreset)
         self.buttonSaveIniPreset.grid(column='3', padx='5', pady='5', row='2')
         self.buttonApplyIniPreset = ttk.Button(self.frameIniSettings, command=self.copyTreeviewToIni)
         self.buttonApplyIniPreset.configure(text='Apply (without saving)')
         self.buttonApplyIniPreset.grid(column='2', columnspan='2', padx='5', pady='5', row='1', sticky='e')
         self.buttonLoadIniPreset = ttk.Button(self.frameIniSettings)
-        self.buttonLoadIniPreset.configure(text='Load Preset')
+        self.buttonLoadIniPreset.configure(text='Load Preset', command=self.loadPreset)
         self.buttonLoadIniPreset.grid(column='3', padx='5', pady='5', row='3')
         self.comboboxLoadIniPreset = ttk.Combobox(self.frameIniSettings)
         self.comboboxLoadIniPreset.grid(column='2', padx='5', pady='5', row='3')
@@ -108,8 +109,9 @@ class CwflyffhelperApp:
         self.frameIniSettings.grid(column='2', columnspan='2', padx='10', pady='10', row='0')
         self.frameLauncher = ttk.Frame(self.toplevel)
         self.pathchooserinputGameFolder = PathChooserInput(self.frameLauncher) # seems like you can't change the width of the ttk.entry within this pygubu.widgets.pathchooserinput :( maybe find a solution in tk instead
-        self.pathchooserinputGameFolder.configure(initialdir='F:\Spiele\Installiert\ClockworksFlyff', path='F:\Spiele\Installiert\ClockworksFlyff', type='directory')
+        self.pathchooserinputGameFolder.configure(initialdir='', path='', type='directory')
         self.pathchooserinputGameFolder.grid(column='5', columnspan='2', row='0')
+        self.pathchooserinputGameFolder.bind('<<PathChooserPathChanged>>', self.event_pathChanged)
         self.buttonLaunchGame = ttk.Button(self.frameLauncher, command=self.launchGame)
         self.buttonLaunchGame.configure(text='Launch Game')
         self.buttonLaunchGame.grid(column='5', padx='5', pady='5', row='1')
@@ -134,6 +136,9 @@ class CwflyffhelperApp:
         self.toplevel.title('CWFlyffHelper')
         self.config = configparser.ConfigParser(allow_no_value=True,delimiters=(' ',), comment_prefixes=('//')) # set space as delimiter instead of =; set // as comments because neuz.ini requires these comments/headers
         self.config.optionxform = str # fix for case-(in)sensitivity
+
+        self.configCWFlyffHelper = configparser.ConfigParser(allow_no_value=True)
+        self.config.optionxform = str # fix for case-(in)sensitivity # probably needed to be compatible with neuz.ini
 
         # Main widget
         self.mainwindow = self.toplevel
@@ -192,12 +197,18 @@ class CwflyffhelperApp:
                 if t.__contains__(filtertext):
                     self.listboxOpenWindows.insert('end', t)
     
+    def event_pathChanged(self, event):
+        self.configCWFlyffHelper.set("OPTIONS", "game_path", self.pathchooserinputGameFolder.cget('path'))
+        with open('cwflyffhelper.ini', 'w') as cwflyffhelperIni:
+            self.configCWFlyffHelper.write(cwflyffhelperIni, True)
+        cwflyffhelperIni.close()
+        self.parseNeuzIni()
 
     def event_treeviewIniSettings(self, event):
         self.entryIniSetting.delete(0, 'end')
         self.entryIniSetting.insert(0, self.treeviewIniSettings.item(self.treeviewIniSettings.focus())['values'][1])
 
-    def edittreeviewIniSettings(self):
+    def edittreeviewIniSettings(self, event):
         selected_item = self.treeviewIniSettings.selection()[0]
         self.treeviewIniSettings.item(selected_item, values=[self.treeviewIniSettings.item(selected_item)['values'][0], self.entryIniSetting.get()]) # keep left column the same and edit the second column
     
@@ -296,6 +307,51 @@ class CwflyffhelperApp:
         self.writeIni()
         self.launchGame()
     
+    def popupHandler(self, message):
+        popup = tk.Toplevel(self.toplevel)
+        popup.geometry("400x100")
+        popupLabel = tk.Label(popup, text=message) #, font= ('Helvetica 12'))
+        popupLabel.pack(pady=30)
+
+    def savePreset(self):
+        if self.comboboxSaveIniPreset.get() == '':
+            self.popupHandler("Preset needs to be named before saving (in box to the left).")
+        else:
+            sectionPreset = "PRESET_" + self.comboboxSaveIniPreset.get()#.capitalize()
+            if(self.configCWFlyffHelper.has_section(sectionPreset) == False): # could also use try/except but whatever
+                self.configCWFlyffHelper.add_section(sectionPreset)
+            self.configCWFlyffHelper.set(sectionPreset, "preset_name", self.comboboxSaveIniPreset.get())
+            # for now, it will just save all settings. In the future I want to change it so that it only saves what has been changed
+            # that way, other settings not part of the preset arent overwritten every time and can be changed ingame still
+            for item in self.treeviewIniSettings.get_children():
+                self.configCWFlyffHelper.set(sectionPreset, self.treeviewIniSettings.item(item)['values'][0], str(self.treeviewIniSettings.item(item)['values'][1]))
+            with open('cwflyffhelper.ini', 'w') as cwflyffhelperIni:
+                self.configCWFlyffHelper.write(cwflyffhelperIni, True)
+            cwflyffhelperIni.close()
+            self.readCWFlyffHelperIni()
+            self.addPresetsToComboboxes()
+            
+    def loadPreset(self):
+        sectionPreset = "PRESET_" + self.comboboxLoadIniPreset.get()
+        for itemPreset in self.configCWFlyffHelper.items(sectionPreset):
+            #replace items in treeviewIniSettings
+            for itemTreeview in self.treeviewIniSettings.get_children():
+                if self.treeviewIniSettings.item(itemTreeview)['values'][0] == itemPreset[0]:
+                    self.treeviewIniSettings.item(itemTreeview, values=[self.treeviewIniSettings.item(itemTreeview)['values'][0], itemPreset[1]])
+
+    def addPresetsToComboboxes(self):
+        # self.comboboxLoadIniPreset['values'], self.comboboxSaveIniPreset['values'] = self.configCWFlyffHelper.sections()[1,'end']
+        try:
+            presetNames = []
+            for section in self.configCWFlyffHelper.sections():
+                if section.__contains__("PRESET_"):
+                    presetNames.append(self.configCWFlyffHelper.get(section, "preset_name"))
+            self.comboboxLoadIniPreset['values'] = presetNames
+            self.comboboxSaveIniPreset['values'] = presetNames
+            #maybe automatically select first item? how?
+        except:
+            print("Couldn't load presets.")
+
     def writeIni(self):
         neuzIniPath = self.pathchooserinputGameFolder.cget('path') + r'\neuz.ini' # maybe have as a class variable instead that updates whenever the path is changed
         with open(neuzIniPath, 'w') as config_file:
@@ -311,6 +367,11 @@ class CwflyffhelperApp:
         neuz.close()
         self.parseNeuzIni() #after saving, load the new ini and put it into treeview
 
+    def readCWFlyffHelperIni(self):
+        self.configCWFlyffHelper.read("cwflyffhelper.ini")
+        self.pathchooserinputGameFolder['path'] = self.configCWFlyffHelper.get("OPTIONS", "game_path")
+        self.addPresetsToComboboxes()
+
     def run(self):
         if self.pathchooserinputGameFolder.cget('path') != '':
             self.parseNeuzIni()
@@ -321,6 +382,7 @@ class CwflyffhelperApp:
 if __name__ == '__main__':
     app = CwflyffhelperApp()
     app.refreshlistboxOpenWindows()
+    app.readCWFlyffHelperIni()
     # app.treeviewIniSettings.insert(parent='', index=0, iid=0, text='', values=('Vineet','Alpha'))
     app.run()
 
